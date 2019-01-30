2a15c631c61b392d8e02cbd9cda76f199704af67
jaki <jumanahmedjaki@gmail.com>
2019-01-30 23:32:42 +0600

    Initial commit

---
+++ app/src/main/java/net/capsulestudio/mikepenznavigationdrawer/menu/TestActivity.java
@@ -0,0 +1,15 @@
+package net.capsulestudio.mikepenznavigationdrawer.menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.capsulestudio.mikepenznavigationdrawer.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
